e6dc6db69c32238387f33b0bb9ac60091b01a29d
cognophile <cognophile@users.noreply.github.com>
2020-05-31 23:49:57 +0100

    Import package dependencies

--- renderer.js
+++ renderer.js
@@ -3,4 +3,7 @@
 // No Node.js APIs are available in this process because
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
-// process.
+// process. 
+window.$ = window.jquery = require("jquery");
+window.popper = require("popper.js");
+require("bootstrap");

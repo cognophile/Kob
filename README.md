e79c6ce258c0dbb17e0028ed142b5c172056a8d7
cognophile <cognophile@users.noreply.github.com>
2020-05-31 16:17:26 +0100

    Add application scaffolding

---
+++ README.md
@@ -0,0 +1,9 @@
+<img src="resources/kob_text.png" alt="kob-icon" width="128" height="36" align="left" hspace="0" vspace="3"/>
+<br><br>
+<img src="resources/kob.png" alt="kob-icon" width="64" height="64" align="right" hspace="0" vspace="3"/>
+
+[![Build Status](https://travis-ci.com/cognophile/Kob.svg?branch=master)](https://travis-ci.com/cognophile/Kob) 
+
+---
+
+A simplified, visual, database-independant, migration tool.
---
+++ app.js
@@ -0,0 +1,118 @@
+'use strict';
+
+const path = require('path');
+const filesystem = require('fs');
+const { app, BrowserWindow, Menu } = require('electron');
+const openAboutWindow = require('about-window').default;
+
+let window
+
+/**
+ * Fetch the window template menu and shortcut bindings
+ * 
+ * @author Cognophile
+ * @returns void
+ */
+function getApplicationTemplateBindings() {
+    return [
+        {
+        label: "Application",
+        submenu: [
+                { 
+                    label: "About", 
+                    click: () => openAboutWindow({
+                        icon_path: path.join(__dirname, 'resources', 'kob.png'),
+                        package_json_dir: __dirname,
+                        product_name: app.getName(),
+                        bug_link_text: 'Found a bug?',
+                        bug_report_url: 'https://github.com/cognophile/Kob/issues/new',
+                        homepage: 'https://github.com/cognophile/Kob',
+                        use_version_info: true,
+                        copyright: 'Copyright (c) cognophile 2020',
+                        adjust_window_size: true,
+                        win_options: {
+                            parent: window,
+                            modal: true,
+                        },
+                        show_close_button: 'Close'
+                    }),
+                    accelerator: "Command+,"
+                },
+                { type: "separator" },
+                { label: "Hide", accelerator: "Command+H", click: function() { app.hide(); }},
+                { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }},
+            ]
+        },
+        {
+            label: "Edit",
+            submenu: [
+                { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+                { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+                { type: "separator" },
+                { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+                { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+                { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+                { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+            ]
+        }
+    ];
+}
+
+/**
+ * Initialise the initial window
+ * 
+ * @author Cognophile
+ * @returns void
+ */
+function initialiseWindow() {
+    window = new BrowserWindow({
+        title: app.getName(),
+        minWidth: 800,
+        minHeight: 600,
+        width: 1600,
+        height: 1000,
+        resizable: true,
+        transparent: false,
+        autoHideMenuBar: true, 
+        titleBarStyle: "hidden",
+        icon: process.platform === 'linux' && path.join(__dirname, 'resources', 'kob.png'),
+        webPreferences: {
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js')
+        },
+    });
+
+    window.loadFile('index.html')
+
+    const document = window.webContents;
+    const appTemplate = getApplicationTemplateBindings()
+    Menu.setApplicationMenu(Menu.buildFromTemplate(appTemplate));
+
+    document.on('dom-ready', () => {
+        if (process.platform === 'darwin') {
+            document.insertCSS(filesystem.readFileSync(path.join(__dirname, 'assets/css/styles.css'), 'utf8'));
+        }
+    });
+
+    window.on('closed', function () {
+        window = null
+    });
+}
+
+app.on('ready', function () {
+    initialiseWindow();
+    window.show();
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (window === null) {
+    initialiseWindow()
+  }
+})
